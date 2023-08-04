a38aadfda087c531620b56900c2dbfd990a05253
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2023-08-04 22:48:02 +0200

    add treetrunkguy

---
+++ scenes/Enemies/TreeTrunkGuy/TreeTrunkGuy.gd
@@ -0,0 +1,24 @@
+extends "res://scripts/entity.gd"
+
+const CLASS = "TreeTrunkGuy"
+
+var behavior_script = load("res://scripts/behaviors/wander.gd")
+var behavior: Node
+
+
+func _ready():
+	super()
+
+	$Interface/Name.text = CLASS
+
+	behavior = behavior_script.new()
+	add_child(behavior)
+
+	behavior.init_wander_and_attack()
+
+	add_item_to_loottable("res://scripts/items/gold.gd", 0.5, 100)
+	add_item_to_loottable("res://scripts/items/healthPotion.gd", 0.25, 1)
+
+
+func fsm(delta):
+	behavior.fsm_wander_and_attack(delta)
